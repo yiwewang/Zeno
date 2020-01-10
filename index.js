59889e5bf15359f1d16e523ddacffc4b0444ead1
Charlize Wang <yiwen.wang@ANSWERREGIONDIE.workdayinternal.com>
2020-01-09 22:26:35 -0800

    survey page set up

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.set('view engine','ejs')
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.render('index'))
+app.post('/', function (req, res) {
+  console.log(req.body.item);
+  res.render('index');  
+})
+app.listen(port, () => console.log(`Zeno listening on port ${port}!`))
+
+
+
+
+
