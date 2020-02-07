32bb3d99709e3ca3edeb7980a722eae31093cbfe
Charlize Wang <yiwen.wang@ANSWERREGIONDIE.workdayinternal.com>
2020-02-06 22:46:49 -0800

    category drop down

--- client/src/Components/ExpenseForm.js
+++ client/src/Components/ExpenseForm.js
@@ -25,7 +25,20 @@ class ExpenseForm extends Component{
 			<form onSubmit={this.handleSubmit}>
         
           <input placeholder="Item" type="text" ref={this.state.item} />
-          <input placeholder="Category" type="text" ref={this.state.category} />
+          <select name="category" class="ghost-input" id="category-select">
+	          <option disabled selected value>Category</option>
+	          <option value="grocery">Groceries</option>
+	          <option value="restaurant">Restaurant</option>
+	          <option value="clothes">Clothes</option>
+	          <option value="other-shopping">Other Shopping</option>
+	          <option value="transportation">Transportation</option>
+	          <option value="entertainment">Entertainment</option>
+	          <option value="medical">Medical</option>
+	          <option value="travel">Travel</option>
+	          <option value="personal-care">Personal Care</option>
+	          <option value="pet">Pet</option>
+	          <option value="fine">Fine</option>
+	        </select>
           <input placeholder="Amount" type="number" ref={this.state.amount} />
           <input placeholder="Tag" type="text" ref={this.state.tag} />
           <input placeholder="Date" type="date" ref={this.state.date} />
